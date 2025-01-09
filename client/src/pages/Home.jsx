b354a4e18e859dad142a6a999ecd426052a32bfd
Anish-Kumar12 <anishkumar344567@gmail.com>
2025-01-09 21:56:14 +0530

    Added valideUrlConvert

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import banner from '../assets/banner.jpg'
+import bannerMobile from '../assets/banner-mobile.jpg'
 
 const Home = () => {
 
   return (
-    <div className='text-red-300'>
-      Home
-    </div>
+    <section className='bg-white'>
+            <div className='container mx-auto'>
+          <div className={`w-full h-full min-h-48 bg-blue-100 rounded ${!banner && "animate-pulse my-2" } `}>
+              <img
+                src={banner}
+                className='w-full h-full hidden lg:block'
+                alt='banner' 
+              />
+              <img
+                src={bannerMobile}
+                className='w-full h-full lg:hidden'
+                alt='banner' 
+              />
+          </div>
+      </div>
+    </section>
   )
 }
 
