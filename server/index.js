f921f2d159a46086cda942d823c40a766639368a
Anish-Kumar12 <anishkumar344567@gmail.com>
2025-01-11 06:34:05 +0530

    Added address routes

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ import uploadRouter from './routes/upload.route.js';
 import subCategoryRouter from './routes/subCategory.route.js';
 import productRouter from './routes/product.route.js';
 import cartRouter from './routes/cart.route.js';
+import addressRouter from './routes/address.route.js';
 dotenv.config();
 
 const app = express();
@@ -37,6 +38,8 @@ app.use("/api/file",uploadRouter)
 app.use("/api/subcategory",subCategoryRouter)
 app.use("/api/product",productRouter)
 app.use("/api/cart",cartRouter)
+app.use("/api/address",addressRouter)
+
 
 
 
