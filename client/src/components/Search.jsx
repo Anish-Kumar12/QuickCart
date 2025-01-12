e6c139e2026ef4deef330fd29af3fbff16bb6cdc
Anish-Kumar12 <anishkumar344567@gmail.com>
2025-01-12 08:33:57 +0530

    Added Checkout page and Add Addresss page

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -2,92 +2,88 @@ import React, { useEffect, useState } from "react";
 import { IoSearch } from "react-icons/io5";
 import { useLocation, useNavigate } from "react-router-dom";
 import { TypeAnimation } from "react-type-animation";
-import { FaArrowLeft } from "react-icons/fa";
-import { Link } from "react-router-dom";
 import useMobile from "../hooks/UseMobile";
+import { Link } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
+
 
 const Search = () => {
-  const navigate = useNavigate();
-  const Location = useLocation();
-  const [isSearchPage, setIsSearchPage] = useState(false);
-  const [isMobile] = useMobile();
+    const navigate = useNavigate();
+    const location = useLocation();
+    const [isSearchPage, setIsSearchPage] = useState(false);
+    const [ isMobile ] = useMobile()
+    const params = useLocation()
+    const searchText = params.search.slice(3)
 
-  useEffect(() => {
-    const isSearch = Location.pathname === "/search";
-    setIsSearchPage(isSearch);
-  }, [Location]);
 
-  const redirecttoSearch = () => {
-    navigate("/search");
-  };
+    useEffect(() => {
+        const isSearch = location.pathname === "/search";
+        setIsSearchPage(isSearch);
+    }, [location]);
 
-  const handleOnChange = (e) => {
-    const value = e.target.value;
-    const url = `/search?q=${value}`;
-    navigate(url);
-  };
-  return (
-    <div className="w-full  min-w-[300px] lg:min-w-[420px] h-11 lg:h-12 rounded-lg border overflow-hidden flex items-center text-neutral-500 bg-slate-50 group focus-within:border-primary-200 ">
-      <div>
-        {isMobile && isSearchPage ? (
-          <Link
-            to={"/"}
-            className="flex justify-center items-center h-full p-2 m-1 group-focus-within:text-primary-200 bg-white rounded-full shadow-md"
-          >
-            <FaArrowLeft size={20} />
-          </Link>
-        ) : (
-          <button className="flex justify-center items-center h-full p-3 group-focus-within:text-primary-200">
-            <IoSearch size={22} />
-          </button>
-        )}
-      </div>
+    const redirectToSearch = () => {
+        navigate("/search");
+    };
 
-      <div>
-        {!isSearchPage ? (
-          <div
-            onClick={redirecttoSearch}
-            className="w-full h-full flex items-center"
-          >
-            <TypeAnimation
-              sequence={[
-                'Search "milk"',
-                1000,
-                'Search "bread"',
-                1000,
-                'Search "sugar"',
-                1000,
-                'Search "panner"',
-                1000,
-                'Search "chocolate"',
-                1000,
-                'Search "curd"',
-                1000,
-                'Search "rice"',
-                1000,
-                'Search "egg"',
-                1000,
-                'Search "chips"',
-              ]}
-              wrapper="span"
-              speed={50}
-              repeat={Infinity}
-            />
-          </div>
-        ) : (
-          <div className="w-full h-full">
-            <input
-              type="text"
-              placeholder="Search for products"
-              autoFocus
-              className="w-full h-full bg-transparent focus:outline-none"
-              onChange={handleOnChange}
-            />
-          </div>
-        )}
-      </div>
-    </div>
-  );
+    const handleOnChange = (e) => {
+        const value = e.target.value;
+        const url = `/search?q=${value}`;
+        navigate(url);
+    };
+
+    return (
+        <div className="w-full min-w-[300px] lg:min-w-[420px] h-12 rounded-lg overflow-hidden flex items-center border text-neutral-600 bg-slate-50 group-focus">
+        <div>
+            {
+                (isMobile && isSearchPage ) ? (
+                    <Link to={"/"} className='flex justify-center items-center h-full p-2 m-1 group-focus-within:text-primary-200 bg-white rounded-full shadow-md'>
+                        <FaArrowLeft size={20}/>
+                    </Link>
+                ) :(
+                    <button className='flex justify-center items-center h-full p-3 group-focus-within:text-primary-200' onClick={redirectToSearch}>
+                        <IoSearch size={22}/>
+                    </button>
+                )
+            }
+        </div>
+            <div className="flex-grow">
+                {!isSearchPage ? (
+                    <div onClick={redirectToSearch} className="w-full h-full flex items-center">
+                        <TypeAnimation
+                            sequence={[
+                                'Search "panner"',
+                                1000,
+                                'Search "chocolate"',
+                                1000,
+                                'Search "curd"',
+                                1000,
+                                'Search "rice"',
+                                1000,
+                                'Search "egg"',
+                                1000,
+                                'Search "chips"',
+                                1000,
+                            ]}
+                            wrapper="span"
+                            speed={50}
+                            repeat={Infinity}
+                        />
+                    </div>
+                ) : (
+                    <div className="w-full h-full">
+                        <input
+                            type="text"
+                            placeholder="Search for products"
+                            autoFocus
+                            defaultValue={searchText}
+                            className="w-full h-full bg-transparent focus:outline-none"
+                            onChange={handleOnChange}
+                        />
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default Search;
